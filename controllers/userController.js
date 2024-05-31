91ec6522aa01104a505258b967fd757bbeae7b6c
Jeremy Permaul <jnpermaul@gmail.com>
2024-05-30 22:53:23 -0700

    added controllers

---
+++ controllers/userController.js
@@ -0,0 +1,143 @@
+const { ObjectId } = require('mongoose').Types;
+const { user, thought } = require('../models');
+
+// Aggregate function to get the number of users overall
+const headCount = async () => {
+  const numberOfusers = await user.aggregate()
+    .count('userCount');
+  return numberOfusers;
+}
+
+// Aggregate function for getting the overall grade using $avg
+const grade = async (userId) =>
+  user.aggregate([
+    // only include the given user by using $match
+    { $match: { _id: new ObjectId(userId) } },
+    {
+      $unwind: '$assignments',
+    },
+    {
+      $group: {
+        _id: new ObjectId(userId),
+        overallGrade: { $avg: '$assignments.score' },
+      },
+    },
+  ]);
+
+module.exports = {
+  // Get all users
+  async getusers(req, res) {
+    try {
+      const users = await user.find();
+
+      const userObj = {
+        users,
+        headCount: await headCount(),
+      };
+
+      res.json(userObj);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+  // Get a single user
+  async getSingleuser(req, res) {
+    try {
+      const user = await user.findOne({ _id: req.params.userId })
+        .select('-__v');
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user with that ID' })
+      }
+
+      res.json({
+        user,
+        grade: await grade(req.params.userId),
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+  // create a new user
+  async createuser(req, res) {
+    try {
+      const user = await user.create(req.body);
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // Delete a user and remove them from the thought
+  async deleteuser(req, res) {
+    try {
+      const user = await user.findOneAndRemove({ _id: req.params.userId });
+
+      if (!user) {
+        return res.status(404).json({ message: 'No such user exists' });
+      }
+
+      const thought = await thought.findOneAndUpdate(
+        { users: req.params.userId },
+        { $pull: { users: req.params.userId } },
+        { new: true }
+      );
+
+      if (!thought) {
+        return res.status(404).json({
+          message: 'user deleted, but no thoughts found',
+        });
+      }
+
+      res.json({ message: 'user successfully deleted' });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+
+  // Add an assignment to a user
+  async addAssignment(req, res) {
+    console.log('You are adding an assignment');
+    console.log(req.body);
+
+    try {
+      const user = await user.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $addToSet: { assignments: req.body } },
+        { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: 'No user found with that ID :(' });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // Remove assignment from a user
+  async removeAssignment(req, res) {
+    try {
+      const user = await user.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $pull: { assignment: { assignmentId: req.params.assignmentId } } },
+        { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: 'No user found with that ID :(' });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
